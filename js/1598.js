a34116e78b90161753da2411bfe4b3ef815e9883
Jatin Gambhir <jatin@eleve.co.in>
2024-07-10 07:59:13 +0530

    Solved 10th July 2024 Logs Min Operation

---
+++ js/1598.js
@@ -0,0 +1,62 @@
+// 1598. Crawler Log Folder
+
+// The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+// The operations are described below:
+
+// "../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+// "./" : Remain in the same folder.
+// "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+// You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
+// The file system starts in the main folder, then the operations in logs are performed.
+
+// Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function (logs) {
+    const operationMap = {
+        "../": "-1",
+        "./": "0"
+    };
+
+    let initial = 0;
+
+    while (logs.length) {
+        let operation = logs.shift();
+        if (operationMap[operation]) {
+            initial += parseInt(operationMap[operation]);
+        } else {
+            initial += 1;
+        }
+        if(initial < 0) {
+            initial = 0
+        }
+    }
+    return initial;
+};
+
+
+let logs;
+
+logs = ["d1/", "d2/", "../", "d21/", "./"]
+
+
+console.log(minOperations(logs));
+
+
+// Example 1:
+// Input: logs = ["d1/","d2/","../","d21/","./"]
+// Output: 2
+// Explanation: Use this change folder operation "../" 2 times and go back to the main folder.
+
+// Example 2:
+// Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
+// Output: 3
+
+// Example 3:
+// Input: logs = ["d1/","../","../","../"]
+// Output: 0
