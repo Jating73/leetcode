6bada463e5d9207e6c30f66c9f47d954aa133f37
Jatin Gambhir <jatin@eleve.co.in>
2024-07-25 14:14:19 +0530

    Solved 25 July 2024 - Merge Sort Implementation

---
+++ js/912.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function (nums) {
+    if (nums.length <= 1) return nums;
+
+    const mid = Math.floor(nums.length / 2);
+    const left = nums.slice(0, mid);
+    const right = nums.slice(mid);
+
+    return merge(sortArray(left), sortArray(right));
+};
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+
+// Given an array of integers nums, sort the array in ascending order and return it.
+
+// You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+ 
+
+// Example 1:
+// Input: nums = [5,2,3,1]
+// Output: [1,2,3,5]
+// Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+
+// Example 2:
+// Input: nums = [5,1,1,2,0,0]
+// Output: [0,0,1,1,2,5]
+// Explanation: Note that the values of nums are not necessairly unique.
